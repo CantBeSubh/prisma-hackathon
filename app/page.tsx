c5872fed959dbc470fadd9348103d75ddfad76dc
Subhranshu Pati <subhranshupati0412@gmail.com>
2024-03-21 19:26:35 +0530

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
-
+type BentoCardProps = {
+  row?: number
+  col?: number
+  children: React.ReactNode
+}
+const BentoCard = (props: BentoCardProps) => {
+  const { row, col, children } = props
+  return (
+    <div className={`col-span-${col ?? 1} row-span-${row ?? 1} bg-gradient-to-r from-neutral-100 to-neutral-200 rounded-lg border-2 border-black flex items-center justify-center p-10 text-black`}>
+      {children ?? 'PLACEHOLDER'}
+    </div>
+  )
+}
 export default function Home() {
   return (
     <>
@@ -6,30 +18,14 @@ export default function Home() {
         <h1 className="text-3xl font-bold underline">Team Pythonic</h1>
         {/* Bento Grid */}
         <div className="grid grid-cols-3 grid-rows-4 gap-4">
-          <div className="col-span-1 row-span-2 bg-gradient-to-r from-blue-500 to-red-500 rounded-lg border-2 border-black p-10 text-white ">
-            USER NAME
-          </div>
-          <div className="col-span-1 row-span-1 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            NO EMAIL BS
-          </div>
-          <div className="col-span-1 row-span-1 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            PRIVACY
-          </div>
-          <div className="col-span-1 row-span-1 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            SECURITY
-          </div>
-          <div className="col-span-1 row-span-2 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            PLACES
-          </div>
-          <div className="col-span-1 row-span-2 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            SERVICES
-          </div>
-          <div className="col-span-1 row-span-2 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            DEVICES
-          </div>
-          <div className="col-span-1 row-span-1 bg-neutral-100 rounded-lg border-2 border-black p-10">
-            CLOUD
-          </div>
+          <BentoCard row={2} col={1}> USER NAME </BentoCard>
+          <BentoCard > NO EMAIL BS </BentoCard>
+          <BentoCard > PRIVACY </BentoCard>
+          <BentoCard > SECURITY </BentoCard>
+          <BentoCard row={2} col={1}> LOCATION </BentoCard>
+          <BentoCard row={2} col={1}> CHAT </BentoCard>
+          <BentoCard row={2} col={1}> DEVICES </BentoCard>
+          <BentoCard> ADD A DEVICE </BentoCard>
         </div>
       </div>
     </>
